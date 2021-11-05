e2758a1305d6eab3f299ff32d21716dc62667485
deivaotv <xdeividsabinox@live.com>
2021-11-05 13:58:19 -0300

    feat: componentizando o content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+//import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -74,22 +73,9 @@ export function App() {
             />
           ))}
         </div>
-
+        
       </nav>
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content movies={movies} selectedGenre={selectedGenre}/>
     </div>
   )
 }
